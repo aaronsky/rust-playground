d58c2c9e68cb84d86876702f87179e2abc001827
Aaron Sky <aaronsky@skyaaron.com>
2018-06-24 13:20:10 -0400

    Improved build process

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 
+PROJECT_DIR=$(git rev-parse --show-toplevel)
+
+rustup override set nightly
+
+rustup target add aarch64-apple-ios
+rustup target add armv7-apple-ios
+rustup target add armv7s-apple-ios
+rustup target add i386-apple-ios
+rustup target add x86_64-apple-ios
+
+echo "export PATH=$HOME/.cargo/bin:$PATH" >> "$PROJECT_DIR/.env"
--- ios/Scripts/build-rust-dependencies.sh
+++ ios/Scripts/build-rust-dependencies.sh
@@ -3,11 +3,20 @@
 PROJECT_ROOT="$PROJECT_DIR/../"
 ENV_FILE="$PROJECT_ROOT/.env"
 
-if [ -f $ENV_FILE ]; then
-    source $ENV_FILE
+if [ -f "$ENV_FILE" ]; then
+    # shellcheck disable=1090
+    # shellcheck disable=2039
+    source "$ENV_FILE"
 fi
 
 RUST_DIR="$PROJECT_ROOT/rust/"
 
-cd $RUST_DIR
-make ios
+cd "$RUST_DIR" || exit 1
+
+if [ "$CONFIGURATION" = "Debug" ]; then
+    echo "Building in debug mode"
+    cargo lipo
+else
+    echo "Building in release mode"
+    cargo lipo --release
+fi
